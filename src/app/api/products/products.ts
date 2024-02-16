156ee676604b8e87758e4953004aaa7ef40fd9e5
Mat <matte971cr7cr8@gmail.com>
2024-02-16 10:27:51 +0100

    test udpate

--- src/app/api/products/products.ts
+++ src/app/api/products/products.ts
@@ -1,7 +1,14 @@
-import { deleteDoc, doc, getDocs, getDoc, collection, addDoc, updateDoc,  } from 'firebase/firestore'
+import {
+  deleteDoc,
+  doc,
+  getDocs,
+  getDoc,
+  collection,
+  addDoc,
+  updateDoc,
+} from 'firebase/firestore'
 import { db } from '../../config/firebase-config'
 import { Products } from '@/app/model/products'
-
 
 export const getProductById = async (productId: string) => {
   const productDocRef = doc(db, 'products', productId)
@@ -20,25 +27,36 @@ export const getProducts = async () => {
 
   const products: Products[] = []
   productsSnapshot.forEach((doc) => {
-    products.push(doc.data() as Products);
-  });
+    products.push(doc.data() as Products)
+  })
 
-  return products;
-};
+  return products
+}
 
 export const createProduct = async (product: Products) => {
   const productsCollectionRef = collection(db, 'products')
   const productDocRef = await addDoc(productsCollectionRef, product)
-  await updateDoc(productDocRef, {id: productDocRef.id})
+  await updateDoc(productDocRef, { id: productDocRef.id })
   return productDocRef.id
 }
 
 export const updateProduct = async (product: Products) => {
-    const productsCollectionRef = collection(db, 'products');
-    const productDocRef = doc(productsCollectionRef, product.id!); 
-    await updateDoc(productDocRef, product);
-    return productDocRef.id;
-};
+  if (!product.id) throw new Error('Product ID is required for update.')
+  const productsCollectionRef = collection(db, 'products')
+  const productDocRef = doc(productsCollectionRef, product.id!)
+
+  const productDataToUpdate: Partial<Products> = {
+    name: product.name,
+    price: product.price,
+    description: product.description,
+    file: product.file,
+    category: product.category,
+    user: product.user,
+  }
+
+  await updateDoc(productDocRef, productDataToUpdate)
+  return productDocRef.id
+}
 
 export const deleteProduct = async (productId: string) => {
   const productDocRef = doc(db, 'products', productId)
