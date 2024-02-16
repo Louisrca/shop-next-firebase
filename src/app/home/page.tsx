590ceade963dc849648a09f03e896c8dbfade6ce
Louis <roccalouis26@gmail.com>
2024-02-16 12:32:34 +0100

    refactor: ui client page

--- src/app/home/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React, { useContext } from "react";
-import { useAuth } from "@/context/AuthUserProvider";
-
-export default function Home() {
-  const { user, logOut } = useAuth();
-
-  return <div>Youre in Home Page {user.email}</div>;
-}
