a4d80bb6740880c7545c4068a0c703095b862814
Louis <roccalouis26@gmail.com>
2024-02-28 10:11:21 +0100

    refactor: new background color when the button is hovered over

--- src/app/(home)/client/component/AddToCard.tsx
+++ src/app/(home)/client/component/AddToCard.tsx
@@ -34,6 +34,7 @@ const AddToCart: React.FC<AddToCartProps> = ({
   return (
     <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
       <Button
+        className="transition duration-500 hover:bg-slate-600/75 hover:text-white"
         aria-label="Ajouter Rapide au panier"
         style={{ width: '100%' }}
         onClick={() => {
